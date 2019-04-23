5e1c37c10e6afbaa14f2d35195af2fed206e1a89
Brandy Mello <brandyamello@gmail.com>
2019-04-23 15:05:48 -0600

    Append card link inputs and clear inputs

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var submitBookmark = document.querySelector('.bookmark-inputs');
+var submit = document.querySelector('.submit-btn')
+var bookmark = document.querySelector('.card-display')
+var bkmkInputs = document.querySelector('.bkmk-input')
+var titleInput = document.querySelector('.title-input');
+var urlInput = document.querySelector('.url-input');
+// var submitBookmark = document.querySelector('.bookmark-inputs');
 
 // submitBookmark.addEventListener('click', function (e) {
 //   var bkmkTitle = document.querySelector('.bkmk-title');
@@ -8,3 +13,28 @@ var submitBookmark = document.querySelector('.bookmark-inputs');
 //   bkmkTitle.value = cardTitle.innerContent;
 //   bkmkAddress.value = cardLink.innerContent;
 // });
+
+submit.addEventListener('click', createBookmark);
+
+function createBookmark(e) {
+  e.preventDefault(e);
+    bookmark.innerHTML += `
+     <div class="bookmark-card">
+      <div class="card-info">
+        <h2 class="card-title">${titleInput.value}</h2>
+        <p class="input-link">${urlInput.value}</p>
+      </div>
+      <div class="card-btns">
+        <button class="mark-read-btn">Read</button>
+        <button class="delete-btn">Delete</button>
+      </div>
+    </div>`
+    clearInputs();
+}
+
+function clearInputs() {
+  titleInput.value = "";
+  urlInput.value = "";
+}
+
+
