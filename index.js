ccb5eb70faf96cd6f2b84f2a9d21466bd646bee9
Brandy Mello <brandyamello@gmail.com>
2019-04-23 10:50:39 -0600

    Work on js

---
+++ index.js
@@ -0,0 +1,10 @@
+var submitBookmark = document.querySelector('.bookmark-inputs');
+
+// submitBookmark.addEventListener('click', function (e) {
+//   var bkmkTitle = document.querySelector('.bkmk-title');
+//   var bkmkAddress = document.querySelector('.bkmk-address');
+//   var cardTitle = document.querySelector('.card-title');
+//   var cardLink = document.querySelector('.input-link');
+//   bkmkTitle.value = cardTitle.innerContent;
+//   bkmkAddress.value = cardLink.innerContent;
+// });
